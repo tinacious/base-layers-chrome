212c89ad4fda826aa2ebd0c7ae979612038e14b4
@tinacious <info@tinaciousdesign.com>
2024-10-19 18:15:35 -0400

    update for manifest v3

--- background.js
+++ background.js
@@ -1,3 +1,24 @@
-chrome.browserAction.onClicked.addListener(function () {
-  chrome.tabs.executeScript(null, { file: 'base-layers.js' });
+chrome.action.onClicked.addListener(() => {
+  handleActionClicked()
 });
+
+async function handleActionClicked() {
+  const tab = await getCurrentTab()
+  const tabId = tab.id
+
+  chrome.scripting.executeScript({
+    target: {
+      tabId,
+    },
+    files: ['base-layers.js']
+  });
+}
+
+async function getCurrentTab() {
+  const tabs = await chrome.tabs.query({
+    active: true,
+    currentWindow: true,
+  })
+
+  return tabs[0]
+}
